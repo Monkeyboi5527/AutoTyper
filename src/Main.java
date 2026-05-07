de056a3e1a12c49bf0bc77cf240e7bc865e27a3e
MonkeyBoi <monkeylaguri308@gmail.com>
2026-05-06 22:29:48 -0400

    JPanel testing

--- src/Main.java
+++ src/Main.java
@@ -19,19 +19,27 @@ void main() {
     // Stops user from resizing window
     window.setResizable(false); // temp
     // Sets background to a certain color
-    window.getContentPane().setBackground(Color.GRAY);
+//    window.getContentPane().setBackground(Color.WHITE);
 
-    // JFrames are the actual window, while JPanels
+    // JFrames are the actual window, while JPanels are like the containers
+    // Default Layout is FlowLayout
     JPanel panel = new JPanel();
-    panel.setLayout(new BorderLayout());
-    panel.setSize(600, 400);
-    panel.setVisible(true);
+    panel.setLayout(new BorderLayout(5, 5));
+    panel.setBackground(Color.YELLOW);
+
+
+    JButton button = new JButton("Button");
+    button.setSize(30, 30);
+    panel.add(button);
+
+
+    window.add(panel);
 
     // JLabels display Images with ImageIcon or Text
     // Creates a new label called "AutoTyper" in the center
     JLabel title = new JLabel("AutoTyper", SwingConstants.CENTER);
     // Adds it to the window and adjusts it to Top center
-    window.add(title, BorderLayout.NORTH);
+    panel.add(title, BorderLayout.PAGE_START);
 
     // Adds Image to window
     // Creates a new ImageIcon with the file location
@@ -40,10 +48,10 @@ void main() {
     // This works too: JLabel kanyeLabel = new JLabel(new ImageIcon("src/assets/kanye64x64.jpg"));
     // Sets the Image automatically or can set it manually ex: (200, 200)
     kanyeLabel.setSize(kanye.getIconWidth(), kanye.getIconHeight());
-    window.add(kanyeLabel);
+//    window.add(kanyeLabel);
 
-    JButton button = new JButton("Button");
-    panel.add(button, BorderLayout.PAGE_START);
+    window.add(panel);
+
 
 
 
